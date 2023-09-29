4713297a18acf9e9c19fe2080c12cd418673aee3
gabri <gabrielrl2004@gmail.com>
2023-09-29 11:25:17 +0200

    Refactorizacion del programa y guardado arreglado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import java.util.ArrayList;
-
 import static org.example.CombinarCorrespondencia.*;
 
 public class Main {
     public static void main(String[] args) {
-        combinarCorrespondencia("template.txt","data.csv");
+        combinarCorrespondencia("template.txt", "data.csv");
     }
 
 
