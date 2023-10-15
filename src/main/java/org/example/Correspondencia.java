d17c83fdf3c346f0f9dcce654c427e4adbc3f887
gabri <gabrielrl2004@gmail.com>
2023-10-15 22:19:54 +0200

    Finalización de app

--- src/main/java/org/example/Correspondencia.java
+++ src/main/java/org/example/Correspondencia.java
@@ -10,8 +10,16 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Esta clase proporciona métodos para combinar correspondencia a partir de una plantilla y un archivo de datos.
+ */
 public class Correspondencia {
-
+    /**
+     * Combina correspondencia a partir de una plantilla y un archivo de datos.
+     *
+     * @param template El nombre del archivo de la plantilla.
+     * @param archivo El nombre del archivo de datos.
+     */
     public static void combinarCorrespondencia(String template, String archivo) {
         comprobarCarpeta();
         String plantilla = leerPlantilla(template);
@@ -28,7 +36,9 @@ public class Correspondencia {
             contador++;
         }
     }
-
+    /**
+     * Comprueba la existencia de la carpeta "salida" y elimina archivos existentes si los hay, o la crea si no existe.
+     */
     private static void comprobarCarpeta() {
         File carpeta = new File("." + File.separator + "salida");
         if (carpeta.exists()) {
@@ -42,7 +52,12 @@ public class Correspondencia {
             carpeta.mkdir();
         }
     }
-
+    /**
+     * Guarda el resultado de la correspondencia en un archivo de salida.
+     *
+     * @param salida El contenido de la correspondencia combinada.
+     * @param id El identificador del archivo de salida.
+     */
     private static void guardarSalida(String salida, String id) {
         try (FileWriter fr = new FileWriter("." + File.separator + "salida" + File.separator + "template-" + id + ".txt")) {
             fr.write(salida);
@@ -50,7 +65,12 @@ public class Correspondencia {
             throw new RuntimeException("Error al guardar el archivo");
         }
     }
-
+    /**
+     * Lee el contenido de la plantilla desde un archivo.
+     *
+     * @param archivo El nombre del archivo de plantilla.
+     * @return El contenido de la plantilla.
+     */
     private static String leerPlantilla(String archivo) {
         String plantilla;
         try {
@@ -60,7 +80,12 @@ public class Correspondencia {
         }
         return plantilla;
     }
-
+    /**
+     * Obtiene los datos a partir de un archivo de datos.
+     *
+     * @param datos El nombre del archivo de datos.
+     * @return Una lista de cadenas de texto representando los datos.
+     */
     private static ArrayList<String> obtenerDatos(String datos) {
         ArrayList<String> filas = new ArrayList<>();
         try (Scanner sc = new Scanner(new File("." + File.separator + datos))) {
@@ -72,7 +97,12 @@ public class Correspondencia {
         }
         return filas;
     }
-
+    /**
+     * Procesa los datos de correspondencia y reemplaza las etiquetas en la plantilla con valores correspondientes.
+     *
+     * @param st Una cadena de datos de correspondencia.
+     * @param plantilla La plantilla con etiquetas a reemplazar.
+     */
     private static void procesarDatos(String st, String plantilla) {
         String[] datos = st.split(",");
         Pattern patron = Pattern.compile("%%(\\d+)%%");
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
-
-
+/**
+ * Esta clase proporciona un punto de entrada para el programa que combina correspondencia.
+ */
 public class Main {
+    /**
+     * Método principal que inicia el programa de combinación de correspondencia.
+     *
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este programa).
+     */
     public static void main(String[] args) {
 
         Correspondencia.combinarCorrespondencia("template.txt", "data.csv");
